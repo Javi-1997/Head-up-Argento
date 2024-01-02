75085ee142202ae4bb059c24a872023c6fac53f4
Javier <javii_lb@hotmail.com>
2024-01-02 20:18:16 -0300

    agregar personaje

--- js/script.js
+++ js/script.js
@@ -1,27 +1,38 @@
-const lista=["Ricardo Darín","Andrea del Boca","Javier Bardem","Luis Brandoni","Soledad Silveyra","Guido Kaczka",
+const lista1=["Ricardo Darín","Andrea del Boca","Javier Bardem","Luis Brandoni","Soledad Silveyra","Guido Kaczka",
 "Carmen Barbieri","Adrian Suar","Ricardo Montaner","Valeria Bertuccelli","Ricardo Fort","Moria Casan","Gastón Pauls",
 "Érica Rivas","Guillermo Franchella","Leonardo Sbaraglia","Susana Giménez","Natalia Oreiro","Diego Peretti","Osvaldo Laport","Sebastian Estebanez",
 "Peter Capusotto"];
+const lista=[];
+
+
 
 let randomString = lista[Math.floor(Math.random() * lista.length)];
 
 console.log(randomString);
 
 function updateCenteredDiv() {
+    setTimeout(function() {
     var randomString = lista[Math.floor(Math.random() * lista.length)];
     var centeredDiv = document.getElementById('centeredDiv');
-    centeredDiv.textContent = randomString;
+    centeredDiv.textContent = randomString;}
+    , 1500);
 }
 
 function addStringToArray() {
-    var newString = prompt("Enter a new string:");
-    if (newString) {
-        lista.push(newString);
-        updateCenteredDiv();
-    }
+    
+    let userInput = prompt("Enter a string:");
+    lista.push(userInput)
+
+ 
+    displayArray();
+
 }
 
 
+function displayArray() {
+    
+    let resultElement = document.getElementById("agregar");
+}
 updateCenteredDiv();
-addStringToArray();
+
 console.log(lista);
